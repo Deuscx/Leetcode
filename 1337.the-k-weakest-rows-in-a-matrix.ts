aef8c9bfe6b4fdbc0948113fa7f6f1503613d5fc
deuscx <1475453714@qq.com>
2023-09-18 21:26:12 +0800

    chore: 1337.the-k-weakest-rows-in-a-matrix

---
+++ 1337.the-k-weakest-rows-in-a-matrix.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1337 lang=typescript
+ * @lcpr version=21913
+ *
+ * [1337] The K Weakest Rows in a Matrix
+ */
+
+// @lc code=start
+function kWeakestRows(mat: number[][], k: number): number[] {
+    const row_sum = []
+    mat.forEach((value, index)=>{
+        row_sum.push([sum(value), index])
+    })    
+    row_sum.sort((a,b)=> a[0] - b[0])
+    return row_sum.map((row)=> row[1]).slice(0,k)
+};
+
+function sum(arr){
+    return arr.reduce((a,b)=> a+b,0)
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[1,0,0,0],[1,1,1,1],[1,0,0,0],[1,0,0,0]]\n2\n
+// @lcpr case=end
+
+ */
+
