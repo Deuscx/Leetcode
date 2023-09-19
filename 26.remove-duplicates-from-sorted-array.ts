c17558d36a7659a081843b26de59b4fe94b49035
deuscx <1475453714@qq.com>
2023-09-20 06:55:16 +0800

    26.remove-duplicates-from-sorted-array

---
+++ 26.remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=26 lang=typescript
+ * @lcpr version=21914
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+function removeDuplicates(nums: number[]): number {
+    let i = 0;
+    let readed = new Set();
+    for (let index = 0; index < nums.length; index++) {
+        const element = nums[index];
+        if (readed.has(element)) {
+            continue
+        }
+
+        readed.add(element)
+        nums[i++] = element
+    }
+    return i
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,0,1,1,1,2,2,3,3,4]\n
+// @lcpr case=end
+
+ */
+
