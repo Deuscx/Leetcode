d65ccf89bef59a8099ee5d25a2d44851083ab7a0
deuscx <1475453714@qq.com>
2023-09-25 14:17:17 +0800

    80.remove-duplicates-from-sorted-array-ii

---
+++ 80.remove-duplicates-from-sorted-array-ii.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=80 lang=typescript
+ * @lcpr version=21917
+ *
+ * [80] Remove Duplicates from Sorted Array II
+ */
+
+// @lc code=start
+function removeDuplicates(nums: number[]): number {
+    let i = 0
+    for (const item of nums) {
+       
+        if(i <=1 || nums[i-2] !== item){
+            nums[i] = item
+            i++;
+        }
+    } 
+    return i
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,1,2,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,0,1,1,1,1,2,3,3]\n
+// @lcpr case=end
+
+ */
+
