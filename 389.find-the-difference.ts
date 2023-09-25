8dccc0e9b05e8070891b5b539f5933933e7a1d1b
deuscx <1475453714@qq.com>
2023-09-25 19:06:32 +0800

    389.find-the-difference

---
+++ 389.find-the-difference.ts
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=389 lang=typescript
+ * @lcpr version=21917
+ *
+ * [389] Find the Difference
+ */
+
+// @lc code=start
+function findTheDifference(s: string, t: string): string {
+    const tMap = t.split('').reduce((acc, cur) =>{
+        if(!acc[cur]){
+            acc[cur] = 1
+        }else{
+            acc[cur] += 1
+        }
+        return acc
+    }, {})
+
+    const sMap = s.split('').reduce((acc, cur) =>{
+        if(!acc[cur]){
+            acc[cur] = 1
+        }else{
+            acc[cur] += 1
+        }
+        
+        return acc
+    }, {})
+
+    for (const char of Object.keys(tMap)) {
+       if(tMap[char] === sMap[char])  continue;
+       return char
+    }
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "abcd"\n"abcde"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// ""\n"y"\n
+// @lcpr case=end
+
+ */
+
