782cb8f9f5802023721c543db2cf3f5352addbbb
deuscx <1475453714@qq.com>
2025-03-10 23:09:34 +0800

    TODO

---
+++ 3306.count-of-substrings-containing-every-vowel-and-k-consonants-ii.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=3306 lang=typescript
+ * @lcpr version=30204
+ *
+ * [3306] 元音辅音字符串计数 II
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+export function countOfSubstrings(word: string, k: number): number {
+
+};
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "aeioqq"\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aeiou"\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "ieaouqqieaouqq"\n1\n
+// @lcpr case=end
+
+ */
