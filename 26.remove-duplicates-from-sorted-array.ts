8711c432084fa8f5c88b4799a3d9985c86c2cc83
deuscx <1475453714@qq.com>
2025-04-02 07:54:07 +0800

    删除有序数组中的重复项

--- 26.remove-duplicates-from-sorted-array.ts
+++ 26.remove-duplicates-from-sorted-array.ts
@@ -5,20 +5,22 @@
  * [26] Remove Duplicates from Sorted Array
  */
 
+// @lcpr-template-start
+
+// @lcpr-template-end
 // @lc code=start
 function removeDuplicates(nums: number[]): number {
-  let i = 0
-  const readed = new Set()
-  for (let index = 0; index < nums.length; index++) {
-    const element = nums[index]
-    if (readed.has(element)) {
-      continue
+  // j始终在开头
+  let j = 0
+  // i 遍历数组
+  for (let i = 1; i < nums.length; i++) {
+    // 将不重复的元素添加到数组开头
+    if (nums[i] !== nums[j]) {
+      j++
+      nums[j] = nums[i]
     }
-
-    readed.add(element)
-    nums[i++] = element
   }
-  return i
+  return j + 1
 };
 // @lc code=end
 
