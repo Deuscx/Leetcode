33a85b1b48c3e97b40fc73d6ae1ae94614aa275e
deuscx <1475453714@qq.com>
2025-06-06 08:30:25 +0800

    轮转数组

--- 189.rotate-array.ts
+++ 189.rotate-array.ts
@@ -5,17 +5,27 @@
  * [189] Rotate Array
  */
 
+// @lcpr-template-start
+
+// @lcpr-template-end
 // @lc code=start
 /**
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-  let step = k % nums.length
-  while (step > 0) {
-    const v = nums.pop() as number
-    nums.unshift(v)
-    step--
+  const step = k % nums.length
+
+  function reverse(nums: number[], start: number, end: number) {
+    while (start < end) {
+      [nums[start], nums[end]] = [nums[end], nums[start]]
+      start++
+      end--
+    }
   }
+
+  reverse(nums, 0, nums.length - 1)
+  reverse(nums, 0, step - 1)
+  reverse(nums, step, nums.length - 1)
 };
 // @lc code=end
 
